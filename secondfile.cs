62a9598dd7b5d9da3484b90eceb5222eec1628be
Shreya Roy <1804606@kiit.ac.in>
2023-01-31 17:24:54 +0530

    This is my second commit

---
+++ secondfile.cs
@@ -0,0 +1,9 @@
+class Employee{
+    public string Name(get; get)
+    public string Age(get; get)
+    public Person(string name; int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
